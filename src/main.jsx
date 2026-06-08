d711c927439f350b2e444d978e5e1c9c80832761
leeyoungjoo <youngjoo0902@naver.com>
2026-06-08 20:30:26 +0900

    푸터 수정, 해시라우터 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import './assets/css/reset.css'
 import './assets/css/layout.css'
@@ -10,7 +10,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.PROD ? '/mystory' : '/'}>
+  <HashRouter basename={import.meta.env.PROD ? '/mystory' : '/'}>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
